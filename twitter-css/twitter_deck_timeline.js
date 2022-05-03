9d813b2c12fe5ce13553d1e9594443d968f44a00
Arash Thr <arashThr@outlook.com>
2022-05-03 17:25:07 +0800

    Remove "run_at"
    No need to do the changes in document_start.
    We can do after loading is finished.

--- twitter-css/twitter_deck_timeline.js
+++ twitter-css/twitter_deck_timeline.js
@@ -1,19 +1,16 @@
-function ready() {
-    let intId
-    intId = setInterval(() => {
-        const targetNode = document.getElementsByClassName('column')
-        if (!targetNode || targetNode.length === 0) return
+let intId
+intId = setInterval(() => {
+    console.log('Checking');
+    const targetNode = document.getElementsByClassName('column')
+    if (!targetNode || targetNode.length === 0) return
 
-        try {
-            targetNode[0].style.display = 'block'
-            targetNode[0].style.width = '50%'
-            targetNode[0].style.margin = '0 auto'
-        } catch (err) {
-            console.error('Setting style for container failed', err)
-        } finally {
-            clearInterval(intId)
-        }
-    }, 500)
-}
-
-document.addEventListener("DOMContentLoaded", ready);
+    try {
+        targetNode[0].style.display = 'block'
+        targetNode[0].style.width = '50%'
+        targetNode[0].style.margin = '0 auto'
+    } catch (err) {
+        console.error('Setting style for container failed', err)
+    } finally {
+        clearInterval(intId)
+    }
+}, 500)
